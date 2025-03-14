b336850667b52053923223d5d933899aee2a05d7
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-03-14 16:22:04 +0100

    added booktitle

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+function init(){
+
+    renderBooksGallery()
+    
+    
+    
+}
+
+
 function renderBooksGallery (i){
 
     for (let i = 0; i < books.length; i++) {
         const element = books[i];
 
-        document.getElementById('book_container').innerHTML += booksGallery(i)
-
-        
+        document.getElementById('book_container').innerHTML += booksGallery(i)        
     }
+
+    renderBookTitle()
+}
+
+function renderBookTitle(indexTitle){
+
+    for (let indexTitle = 0; indexTitle < books.length; indexTitle++) {
+        // let bookName = books[0].name[indexTitle];
+        
+       let bookName = document.getElementById('bookTitle');
+       
+       
+
+        console.log(bookName);
+    }
+
+    
+    
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,7 +1,13 @@
 function booksGallery(i){
 
     return /*html*/`
-        <div id="book${i + 1}" class="book"></div>
-        
+        <div id="book${i + 1}" class="book">
+        <div id="bookTitle${i + 1}" class="bookTitle">${books[i].name}</div>
+
+
+        </div>      
         `
-}
+    }
+
+
+
